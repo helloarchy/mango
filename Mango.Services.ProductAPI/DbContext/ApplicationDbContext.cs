a21d6ffa69e018736290e851a81561d43a436c0d
Robert Hardy <helloarchy@gmail.com>
2022-01-27 21:01:25 +0000

    Add DB context with connection string

---
+++ Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.ProductAPI.DbContext;
+
+public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    
+    
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,3 +1,5 @@
+using Mango.Services.ProductAPI.DbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
