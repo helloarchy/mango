33217a984a48f7790887240f588a6d16d4488cee
Robert Hardy <helloarchy@gmail.com>
2022-01-27 21:40:55 +0000

    Add products model and update context

--- Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.DbContext;
+﻿using Mango.Services.ProductAPI.Models;
+
+namespace Mango.Services.ProductAPI.DbContext;
 
 public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
     }
     
-    
+    public DbSet<Product> Products { get; set; }
 }
