57fb1d2a0390c2cf1a1ff257508cd1bdd974e7d9
Robert Hardy <hardyrar@gmail.com>
2022-02-04 22:12:25 +0000

    Seed products, add migrations

--- Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
+++ Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -21,7 +21,7 @@ public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
             Price = 15,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://mangostorageacc.blob.core.windows.net/mango/14.jpg",
             CategoryName = "Appetizer"
         });
 
@@ -32,7 +32,7 @@ public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
             Price = 13.99,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://mangostorageacc.blob.core.windows.net/mango/12.jpg",
             CategoryName = "Appetizer"
         });
 
@@ -43,7 +43,7 @@ public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
             Price = 10.99,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://mangostorageacc.blob.core.windows.net/mango/11.jpg",
             CategoryName = "Dessert"
         });
 
@@ -54,7 +54,7 @@ public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
             Price = 15,
             Description =
                 "Praesent scelerisque, mi sed ultrices condimentum, lacus ipsum viverra massa, in lobortis sapien eros in arcu. Quisque vel lacus ac magna vehicula sagittis ut non lacus.<br/>Sed volutpat tellus lorem, lacinia tincidunt tellus varius nec. Vestibulum arcu turpis, facilisis sed ligula ac, maximus malesuada neque. Phasellus commodo cursus pretium.",
-            ImageUrl = "",
+            ImageUrl = "https://mangostorageacc.blob.core.windows.net/mango/13.jpg",
             CategoryName = "Entree"
         });
     }
