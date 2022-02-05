fae360f05375fcedf3596cafb595227030f0157f
Robert Hardy <helloarchy@gmail.com>
2022-02-05 17:34:45 +0000

    Reference products api and add dto's

---
+++ Mango.Web/SD.cs
@@ -0,0 +1,16 @@
+﻿namespace Mango.Web;
+
+/// <summary>
+/// Static details class to hold all constants
+/// </summary>
+public class SD
+{
+    public string ProductAPIBase { get; set; }
+    public enum ApiType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+}
