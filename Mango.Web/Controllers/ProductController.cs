ed128d0e5243a46db8f8607775b67c1294006eb6
Robert Hardy <helloarchy@gmail.com>
2022-02-07 19:23:15 +0000

    Get products from api

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<ViewResult> ProductIndex()
+    {
+        var list = new List<ProductDto>();
+        var response = await _productService.GetAllProductsAsync<ResponseDto>();
+
+        if (response is not null && response.IsSuccess)
+        {
+            var result = Convert.ToString(response.Result);
+            list = JsonConvert.DeserializeObject<List<ProductDto>>(result);
+        }
+        
+        return View(list);
+    }
+}
